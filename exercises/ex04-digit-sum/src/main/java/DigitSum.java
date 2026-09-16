1d4b335e6aa4e2363648fe82e6e1185b781c8ee7
James Fu <jfu0952@gmail.com>
2026-09-16 14:18:28 -0400

    Complete DigitSum exercise

--- exercises/ex04-digit-sum/src/main/java/DigitSum.java
+++ exercises/ex04-digit-sum/src/main/java/DigitSum.java
@@ -21,7 +21,22 @@ public class DigitSum {
      * @return the sum of its decimal digits
      */
     public static int digitSum(int n) {
-        // TODO: complete
-        return 0;
+//        String digitString = String.valueOf(Math.abs(n));
+//        int sum = 0;
+//        for (int i = 0; i < digitString.length(); i++) {
+//            sum += Character.getNumericValue(digitString.charAt(i));
+//        }
+//        return sum;
+        // This solution is kinda funky, I don't like it very much.
+
+        int num = Math.abs(n);
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+        // This solution is arguably more clean but gets into number construction funnies.
+        // Also, it's ever so slightly slower (?)
     }
 }
